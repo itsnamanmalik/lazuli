7b14fa0d0b72bfa0d90995e25f5c214a04dcd450
Naman Malik <namanmalik06@gmail.com>
2022-01-29 16:27:04 +0530

    as view

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from website.views import *
 
 urlpatterns = [
-        path('', Index.as_view,name="index"),
+        path('', Index.as_view(),name="index"),
 ]
