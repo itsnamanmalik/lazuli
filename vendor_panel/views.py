99777d029938adfa2599a01b9a3ed0097cc31631
Naman Malik <namanmalik06@gmail.com>
2022-06-11 03:09:53 +0530

    added vendor panel pages

--- vendor_panel/views.py
+++ vendor_panel/views.py
@@ -4,16 +4,33 @@ from django.contrib import messages
 from django.shortcuts import redirect
 
 
-
 class Dashboard(View):
     def get(self, request):
         return render(request,'vendor/dashboard.html')
+    
         
 class VendorLogin(View):
     def get(self, request):
         return render(request,'vendor/login.html')
     
+        
+class Sales(View):
+    def get(self, request):
+        return render(request,'vendor/sales.html')
     
+        
+class MyProfile(View):
+    def get(self, request):
+        return render(request,'vendor/my-profile.html')
     
+        
+class ResetPassword(View):
+    def get(self, request):
+        return render(request,'vendor/reset-pass.html')
+    
+        
+class AddSales(View):
+    def get(self, request):
+        return render(request,'vendor/add-sales.html')
     
     
